7e1589ac0bdaae63fefe71beb5323d0137852a2f
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-07-28 19:26:10 +0300

    link

--- src/main/java/a/b/c/MyBean.java
+++ src/main/java/a/b/c/MyBean.java
@@ -3,7 +3,7 @@ package a.b.c;
 /**
  * *
  * <p>beans for this class are defined in xml config and in java config</p>
- * <p>https://youtrack.jetbrains.com/issue/IDEA-274549</p>
+ * <p>https://youtrack.jetbrains.com/issue/IDEA-100839</p>
  * *
  */
 public class MyBean {
